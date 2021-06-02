30d86a4b4b0690d132d4233bb749161d91c3b585
jacek-domanski <j.domanski92@gmail.com>
2021-06-02 21:09:33 +0200

    Add kookaburra path reading from file

--- FourierTest/src/sample/Kookaburra.java
+++ FourierTest/src/sample/Kookaburra.java
@@ -4,40 +4,66 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Kookaburra {
     public static ArrayList<ComplexNumber> generate() throws IOException {
-        String fileName = "C:\\Java\\FourierTransformExperiments\\FourierTest\\src\\sample\\kookaburra_path_sample.txt";
-        File file = new File(fileName);
+        String fileName = "C:\\Java\\FourierTransformExperiments\\FourierTest\\src\\sample\\kookaburra_path.txt";
+        ArrayList<ComplexNumber> signals = readFile(fileName);
+
+        ComplexNumber offset = calculateOffset(signals);
+        ComplexNumber scale = new ComplexNumber(0.8, 0);
+
+        applyOffsetAndScale(signals, offset, scale);
+
+        return signals;
+    }
+
+    private static ArrayList<ComplexNumber> readFile(String fileName) throws IOException {
         BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(fileName));
 
-        bufferedReader.lines().forEach(System.out::println);
+        return bufferedReader.lines()
+                .map(line -> line.split(","))
+                .map(coordsStr -> Arrays.stream(coordsStr)
+                        .map(Double::parseDouble)
+                        .toArray(Double[]::new))
+                .map(coords -> new ComplexNumber(coords[0], coords[1]))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
 
-        ArrayList<ComplexNumber> signals = new ArrayList<>();
+    private static ComplexNumber calculateOffset(ArrayList<ComplexNumber> signals) {
+        ComplexNumber min = signals.get(0).copy();
+        ComplexNumber max = signals.get(0).copy();
 
+        getMinAndMax(signals, min, max);
 
-        double minRe = signals.get(0).getRe();
-        double minIm = signals.get(0).getIm();
-        double maxRe = signals.get(0).getRe();
-        double maxIm = signals.get(0).getIm();
+        double reOffset = min.getRe() + ((max.getRe() - min.getRe()) / 2);
+        double imOffset = min.getIm() + ((max.getIm() - min.getIm()) / 2);
 
-        for (int i = 0; i < signals.size(); i++) {
-            minRe = Math.min(minRe, signals.get(i).getRe());
-            minIm = Math.min(minIm, signals.get(i).getIm());
+        return new ComplexNumber(reOffset, imOffset);
+    }
 
-            maxRe = Math.max(maxRe, signals.get(i).getRe());
-            maxIm = Math.max(maxIm, signals.get(i).getIm());
+    private static void getMinAndMax(ArrayList<ComplexNumber> signals, ComplexNumber min, ComplexNumber max) {
+        for (ComplexNumber signal : signals) {
+            if (signal.getRe() < min.getRe()) {
+                min.setRe(signal.getRe());
+            } else if (signal.getRe() > max.getRe()) {
+                max.setRe(signal.getRe());
+            }
+
+            if (signal.getIm() < min.getIm()) {
+                min.setIm(signal.getIm());
+            } else if (signal.getIm() > max.getIm()) {
+                max.setIm(signal.getIm());
+            }
         }
+    }
 
-        double reOffset =minRe + ((maxRe - minRe) / 2);
-        double imOffset =minIm + ((maxIm - minIm) / 2);
-
-        for (int i = 0; i < signals.size(); i++) {
-            signals.set(i, new ComplexNumber(
-                    (signals.get(i).getRe() - reOffset)*0.8,
-                    (signals.get(i).getIm() - imOffset)*0.8));
-        }
-        return signals;
+    private static void applyOffsetAndScale(ArrayList<ComplexNumber> signals, ComplexNumber offset, ComplexNumber scale) {
+        signals.forEach(signal -> {
+            signal.sub(offset);
+            signal.multiply(scale);
+        });
     }
 }
